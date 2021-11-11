55e814c1bf780601ef5980deeeeda05eafc9115f
Marysia Winkels <marysia@live.nl>
2021-11-11 14:49:43 +0100

    add

---
+++ assets/GDD/hide.js
@@ -0,0 +1,11 @@
+code_show=true; 
+function code_toggle() {
+    if (code_show){
+        $('div.cell.code_cell.rendered.selected div.input').hide();
+    } else {
+        $('div.cell.code_cell.rendered.selected div.input').show();
+    }
+    code_show = !code_show
+} 
+
+$( document ).ready(code_toggle);
